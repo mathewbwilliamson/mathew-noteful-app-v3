6f55d24200f9a2219c43f3bf76436d163f5cff8d
Matt Williamson <mathewbwilliamson@gmail.com>
2018-09-18 15:20:22 -0400

    finished scratch Create New Note Query

--- scratch/queries.js
+++ scratch/queries.js
@@ -33,16 +33,42 @@ const Note = require('../models/note');
 ////////////////////////////////////////////////////////////////
 //FindbyId
 ////////////////////////////////////////////////////////////////
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const searchId = '000000000000000000000004'; //7 things lady gaga has in common with cats
+//     let filter = {};
+
+//     // if (searchTerm) {
+//     //   filter.title = { $regex: searchTerm, $options: 'gi' };
+//     // }
+
+//     return Note.findById(searchId).sort({ updatedAt: 'desc' });
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+////////////////////////////////////////////////////////////////
+//Create a New Note
+////////////////////////////////////////////////////////////////
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => {
-    const searchId = '000000000000000000000004'; //7 things lady gaga has in common with cats
-    let filter = {};
+    const inputObj = {title: '72 lessons learned from cats',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      createdAt: new Date('January 1, 2015')
+    };
 
-    // if (searchTerm) {
-    //   filter.title = { $regex: searchTerm, $options: 'gi' };
-    // }
-
-    return Note.findById(searchId).sort({ updatedAt: 'desc' });
+    console.log(inputObj.createdAt)
+    console.log(inputObj.updatedAt)
+    
+    return Note.create(inputObj);
   })
   .then(results => {
     console.log(results);
