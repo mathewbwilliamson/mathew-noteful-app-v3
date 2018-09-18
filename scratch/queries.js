08ae8a4a6f5a5ae2e0d255567b780a6695592403
Matt Williamson <mathewbwilliamson@gmail.com>
2018-09-18 15:43:39 -0400

    Added Delete to Scratch Queries.js

--- scratch/queries.js
+++ scratch/queries.js
@@ -58,20 +58,60 @@ const Note = require('../models/note');
 ////////////////////////////////////////////////////////////////
 //Create a New Note
 ////////////////////////////////////////////////////////////////
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const inputObj = {title: '72 lessons learned from cats',
+//       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+//       createdAt: new Date('January 1, 2015')
+//     };
+
+//     return Note.create(inputObj);
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+////////////////////////////////////////////////////////////////
+//Update a Note By Id using Note.findByIdAndUpdate
+////////////////////////////////////////////////////////////////
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const searchId = '000000000000000000000004';
+//     const inputObj = {title: '42 lessons learned from cats',
+//       content: 'Smaller Lorem, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+//     };
+
+//     return Note.findByIdAndUpdate(searchId, inputObj, {new:true});
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+////////////////////////////////////////////////////////////////
+//Delete a note by id using Note.findByIdAndRemove
+////////////////////////////////////////////////////////////////
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => {
-    const inputObj = {title: '72 lessons learned from cats',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      createdAt: new Date('January 1, 2015')
-    };
+    const searchId = '000000000000000000000004';
 
-    console.log(inputObj.createdAt)
-    console.log(inputObj.updatedAt)
-    
-    return Note.create(inputObj);
+    return Note.findByIdAndRemove(searchId);
   })
-  .then(results => {
-    console.log(results);
+  .then( () => {
+    console.log('Status should be 204');
   })
   .then(() => {
     return mongoose.disconnect()
