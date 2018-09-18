fc70a8a4cdfcb36b32495d30104072e991d43765
Matt Williamson <mathewbwilliamson@gmail.com>
2018-09-18 14:28:24 -0400

    Created models/notes.hjs

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  PORT: process.env.PORT || 8080
+  PORT: process.env.PORT || 8080,
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://mbw:mongopass1@ds261342.mlab.com:61342/noteful-app-mw'
 };
---
+++ models/notes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: String
+});
+
+noteSchema.set('timestamps', true);
