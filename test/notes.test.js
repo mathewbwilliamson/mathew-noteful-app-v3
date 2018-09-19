a54963755511fbe086f6e18fcf118ac3c946d575
Matt Williamson <mathewbwilliamson@gmail.com>
2018-09-19 14:48:09 -0400

    Added notes.test

--- test/notes.test.js
+++ test/notes.test.js
@@ -14,3 +14,36 @@ const { notes } = require('../db/seed/notes');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
+describe('Reality Check', () => {
+
+  it('true should be true', () => {
+    expect(true).to.be.true;
+  });
+
+  it('2 + 2 should equal 4', () => {
+    expect(2 + 2).to.equal(4);
+  });
+
+});
+
+describe('All tests are here', () => {
+
+  before(function () {
+    return mongoose.connect(TEST_MONGODB_URI)
+      .then(() => mongoose.connection.db.dropDatabase());
+  });
+
+  beforeEach(function () {
+    return Note.insertMany(notes);
+  });
+
+  afterEach(function () {
+    return mongoose.connection.db.dropDatabase();
+  });
+
+  after(function () {
+    return mongoose.disconnect();
+  });
+
+
+})
