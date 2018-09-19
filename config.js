978acace1796a5c29882ab90eb6cca9143710194
Matt Williamson <mathewbwilliamson@gmail.com>
2018-09-19 14:42:08 -0400

    Adding CI integration

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://mbw:mongopass1@ds261342.mlab.com:61342/noteful-app-mw'
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://mbw:mongopass1@ds261342.mlab.com:61342/noteful-app-mw',
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27107/noteful-test'
 };
